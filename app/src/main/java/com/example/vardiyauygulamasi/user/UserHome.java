5b9d6e76804031753b288632c90389067a4ac8ac
M-Taha-Kirkgoz <m.taha.kirkgoz@gmail.com>
2024-01-21 16:49:09 +0300

    Update UserHome.java
    Kullanıcılar için vardiya sayfası tamamlandı.

--- app/src/main/java/com/example/vardiyauygulamasi/user/UserHome.java
+++ app/src/main/java/com/example/vardiyauygulamasi/user/UserHome.java
@@ -1,16 +1,66 @@
 package com.example.vardiyauygulamasi.user;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
+import com.example.vardiyauygulamasi.Adapters.ShiftViewAdapter;
+import com.example.vardiyauygulamasi.DatabaseHelper;
+import com.example.vardiyauygulamasi.Dtos.Shift;
+import com.example.vardiyauygulamasi.Dtos.User;
 import com.example.vardiyauygulamasi.R;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
 public class UserHome extends AppCompatActivity {
+    private int selectedDepartmentId = -1;
+    private String selectedDate;
+    DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_home);
+        setContentView(R.layout.shifts_show);
+
+        db = new DatabaseHelper(getApplicationContext());
+
+        Intent intent = getIntent();
+
+        User userDetail = (User) intent.getSerializableExtra("userDetail");
+
+        Toast.makeText(this, String.valueOf(userDetail.departmentId), Toast.LENGTH_SHORT).show();
+
+        selectedDepartmentId = userDetail.departmentId;
+
+        if (selectedDepartmentId != -1){
+            CalendarView date = findViewById(R.id.vardiya_tarih);
+            RecyclerView table = findViewById(R.id.liste);
+
+            date.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+                @Override
+                public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.set(year, month, dayOfMonth);
+
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                    selectedDate = dateFormat.format(calendar.getTime());
+
+                    ArrayList<Shift> shifts = db.getAllShiftByDateAndDepartmentId(selectedDate, selectedDepartmentId);
+
+                    table.setLayoutManager(new LinearLayoutManager(UserHome.this));
+
+                    ShiftViewAdapter viewAdapter = new ShiftViewAdapter(shifts);
+                    table.setAdapter(viewAdapter);
+                }
+            });
+        }
     }
 }
