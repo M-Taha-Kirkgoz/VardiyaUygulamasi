ff39d07cac50295605e45e75acb7f5a6a59d0175
M-Taha-Kirkgoz <m.taha.kirkgoz@gmail.com>
2024-01-25 21:31:26 +0300

    Dialog yapısı kuruldu.

--- app/src/main/java/com/example/vardiyauygulamasi/MainActivity.java
+++ app/src/main/java/com/example/vardiyauygulamasi/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.vardiyauygulamasi;
 
+import android.app.ActionBar;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,9 +17,13 @@ import com.example.vardiyauygulamasi.admin.AdminHome;
 import com.example.vardiyauygulamasi.Dtos.User;
 import com.example.vardiyauygulamasi.user.UserHome;
 
+import org.w3c.dom.Text;
+
 
 public class MainActivity extends AppCompatActivity {
     DatabaseHelper db;
+
+    Dialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +38,24 @@ public class MainActivity extends AppCompatActivity {
 
         Button deleteTable = findViewById(R.id.tablo_sil);
         Button createTable = findViewById(R.id.tablo_olustur);
+
+        dialog = new Dialog(MainActivity.this);
+        dialog.setContentView(R.layout.custom_alert_box);
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.custom_dialog_bg));
+        dialog.setCancelable(false);
+
+        TextView title = dialog.findViewById(R.id.title);
+        TextView body = dialog.findViewById(R.id.body);
+
+        Button btnDialogOk = dialog.findViewById(R.id.btnDialogOk);
+
+        btnDialogOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
 
         deleteTable.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +75,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 boolean kullanici_varmi = db.userIsHave(Long.parseLong(tc_no.getText().toString()));
+
+                dialog.show();
 
                 if (kullanici_varmi){
                     boolean login = db.userLogin(Long.parseLong(tc_no.getText().toString()), pass.getText().toString());
@@ -70,12 +98,20 @@ public class MainActivity extends AppCompatActivity {
                         startActivity(intnt);
                     }
 
-                    else
-                        Toast.makeText(MainActivity.this, "Kullanici Girisi Basarisiz", Toast.LENGTH_LONG).show();
+                    else{
+                        title.setText("Kullanıcı girişi başarısız !");
+                        body.setText("T.C. numarası veya şifre hatalı.");
+
+                        dialog.show();
+                    }
                 }
 
-                else
-                    Toast.makeText(MainActivity.this, "Boyle Bir Kullanici Bulunamadi " + Long.parseLong(tc_no.getText().toString()), Toast.LENGTH_LONG).show();
+                else {
+                    title.setText("Kullanıcı Bulunamadı !");
+                    body.setText(Long.parseLong(tc_no.getText().toString()) + " T.C. numarasına kayıtlı herhangi bir kullanıcı bulunamadı.");
+
+                    dialog.show();
+                }
             }
         });
 
